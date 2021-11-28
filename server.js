ee03762de77d208b5bddfba5c952459c061092e9
Hamza Koc <hamzakooc@gmail.com>
2021-11-28 13:00:05 -0500

    port updated

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const employeeRouter = require('./routes/EmployeeRoutes.js');
-// var cors = require('cors')
+var cors = require('cors')
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // Make sure it comes back as json
+app.use(cors())
 
-
-const port = process.env.PORT || 9090;
-
+//TODO - Replace you Connection String here
 
 mongoose.connect('mongodb+srv://std:std1234@cluster0.b1201.mongodb.net/101274109_assignment2',
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
-
+// mongoose.connect('mongodb://localhost:27017/myapp');
 
 
 app.use(employeeRouter);
 
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
 
-//local
-// app.listen(9090, () => { console.log('Server is running http://localhost:9090/') })
+const port = process.env.PORT || 9090
+app.listen(port, () => { console.log('Server is running http://localhost:9090/') })
