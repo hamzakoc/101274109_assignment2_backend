024823114132984841529b6138fd0ea4fc285708
Hamza Koc <hamzakooc@gmail.com>
2021-11-27 21:47:09 -0500

    port updated

--- server.js
+++ server.js
@@ -1,30 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const employeeRouter = require('./routes/EmployeeRoutes.js');
-var cors = require('cors')
+// var cors = require('cors')
 
 const app = express();
 app.use(express.json());
 
 
 const port = process.env.PORT || 9090;
-
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,
-  optionSuccessStatus: 200
-}
-
-
-app.use(cors(corsOptions))
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 
 mongoose.connect('mongodb+srv://std:std1234@cluster0.b1201.mongodb.net/101274109_assignment2',
@@ -41,5 +24,4 @@ app.listen(port, () => {
 });
 
 //local
-
 // app.listen(9090, () => { console.log('Server is running http://localhost:9090/') })
