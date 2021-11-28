12af755dc622febbf2a7bb055f2c6e594b552945
Hamza Koc <hamzakooc@gmail.com>
2021-11-27 20:35:26 -0500

    corse deactivated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const employeeRouter = require('./routes/EmployeeRoutes.js');
-var cors = require('cors')
+// var cors = require('cors')
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
 
 
 
