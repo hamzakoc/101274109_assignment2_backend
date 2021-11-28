584be01586e3297570ee4f77b5e961f2ac2a190a
Hamza Koc <hamzakooc@gmail.com>
2021-11-27 20:12:28 -0500

    backend finalized

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const employeeRouter = require('./routes/EmployeeRoutes.js');
+var cors = require('cors')
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+
+
+mongoose.connect('mongodb+srv://std:std1234@cluster0.b1201.mongodb.net/101274109_assignment2',
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+
+
+
+app.use(employeeRouter);
+
+app.listen(9090, () => { console.log('Server is running http://localhost:9090/') })
